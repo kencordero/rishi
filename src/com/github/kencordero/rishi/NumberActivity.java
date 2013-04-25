d587cfb5d93870c4eb10466cdfd2146d2528b0cc
Kenneth Cordero <kencordero@gmail.com>
2013-04-24 22:40:44 -0500

    Added activity class, string title resource and manifest entry

---
+++ src/com/github/kencordero/rishi/NumberActivity.java
@@ -0,0 +1,14 @@
+package com.github.kencordero.rishi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NumberActivity extends Activity {
+	public final String TAG = "NumberActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO inflate layout
+	}
+}
