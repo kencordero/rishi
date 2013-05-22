dba77409fa5b9e95bb3f10d9bd143eda3187512e
Kenneth Cordero <kencordero@gmail.com>
2013-05-21 23:36:04 -0500

    Set up locale array adapter

--- src/com/github/kencordero/rishi/SettingsActivity.java
+++ src/com/github/kencordero/rishi/SettingsActivity.java
@@ -1,18 +1,28 @@
 package com.github.kencordero.rishi;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class SettingsActivity extends ListActivity {
+	Resources res = getResources();
+	String locales[] = res.getStringArray(R.array.locales);
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		Resources res = getResources();
+		String locales[] = res.getStringArray(R.array.locales);
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, locales));
 	}
 	
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
+		String localeName = locales[position];
+		//TODO set locale
+		
 	}
 }
