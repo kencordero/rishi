e2788e926d2cc429ac7b8e646fa3ea02033d6626
Kenneth Cordero <kencordero@gmail.com>
2014-04-14 21:38:10 -0500

    separate OnInitListener for each TTS

--- src/com/kentheken/rishi/TTSEngine.java
+++ src/com/kentheken/rishi/TTSEngine.java
@@ -5,24 +5,42 @@ import java.util.Locale;
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
 
-public class TTSEngine implements OnInitListener {
+public class TTSEngine {
+	private static final String TAG = "TTSEngine";
 	private static TextToSpeech mTTS_en;
 	private static TextToSpeech mTTS_mr;
 	private static TextToSpeech mTTS_es;
 	
 	public TTSEngine(Context c) {
 		if (mTTS_en == null) {
-			mTTS_en = new TextToSpeech(c, this);
-			mTTS_en.setLanguage(Locale.ENGLISH);
+			mTTS_en = new TextToSpeech(c, new OnInitListener() {
+				@Override
+				public void onInit(int status) {
+					mTTS_en.setLanguage(Locale.ENGLISH);
+					Log.d(TAG, "English TTS initialized");
+				}				
+			});			
 		}
 		if (mTTS_mr == null) {
-			mTTS_mr = new TextToSpeech(c, this);
-			mTTS_mr.setLanguage(new Locale("hi"));
+			mTTS_mr = new TextToSpeech(c, new OnInitListener() {
+				@Override
+				public void onInit(int status) {
+					mTTS_mr.setLanguage(new Locale("hi"));
+					Log.d(TAG, "Marathi TTS initialized");
+				}
+				
+			});			
 		}
 		if (mTTS_es == null) {
-			mTTS_es = new TextToSpeech(c, this);
-			mTTS_es.setLanguage(new Locale("es"));
+			mTTS_es = new TextToSpeech(c, new OnInitListener() {
+				@Override
+				public void onInit(int status) {
+					mTTS_es.setLanguage(new Locale("es"));
+					Log.d(TAG, "Spanish TTS initialized");
+				}				
+			});			
 		}
 	}
 	
@@ -33,9 +51,7 @@ public class TTSEngine implements OnInitListener {
 			mTTS_mr.stop();
 		if (mTTS_es != null)
 			mTTS_es.stop();
-	}
-	
-	public void onInit(int status) { }
+	}		
 	
 	public void speak(FlashCardFragment2.LocaleId lId, String textToSpeak) {
 		stop();
@@ -51,8 +67,7 @@ public class TTSEngine implements OnInitListener {
 			tts = mTTS_es;
 			break;
 		}
-		tts.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null);
-		
+		tts.speak(textToSpeak, TextToSpeech.QUEUE_ADD, null);		
 	}
 	
 	public void shutdown() {
