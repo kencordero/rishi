bd19ac2d7e2e7e083044dc5844b5472beb092aa7
Kenneth Cordero <kencordero@gmail.com>
2015-06-29 17:14:04 -0500

    Added some new images for colors

--- app/src/main/java/com/kentheken/rishi/DatabaseOpenHelper.java
+++ app/src/main/java/com/kentheken/rishi/DatabaseOpenHelper.java
@@ -37,18 +37,18 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper {
         return sDbHelper;
     }
 
-	  /**
-	     * Creates a empty database on the system and rewrites it with your own database.
-	     * */
-	    public void createDatabase() {
+    /**
+     * Creates a empty database on the system and rewrites it with your own database.
+     * */
+    public void createDatabase() {
+        boolean dbExists = checkDatabase();
 
-	        boolean dbExists = checkDatabase();
-            if (!dbExists) {
-	            // By calling this method, an empty database will be created into the default system path
-	            // of your application so we are able to overwrite that database with ours.
-	            createNewDatabase();
-	        }
-	    }
+        if (!dbExists) {
+	        // By calling this method, an empty database will be created into the default system path
+	        // of your application so we are able to overwrite that database with ours.
+	        createNewDatabase();
+        }
+    }
 
     private void createNewDatabase() {
         this.getReadableDatabase();
@@ -61,82 +61,81 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper {
         }
     }
 
-	    /**
-	     * Check if the database already exist to avoid re-copying the file each time you open the application.
-	     * @return true if it exists, false if it doesn't
-	     */
-	    private boolean checkDatabase(){
-	        SQLiteDatabase database = null;
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDatabase(){
+        SQLiteDatabase database = null;
 
-	        try {
-	            database = SQLiteDatabase.openDatabase(getPath(), null, SQLiteDatabase.OPEN_READONLY);
-	        } catch(SQLiteException e) {
-	        	Log.e(this.getClass().toString(), "Error while checking db");	            
-	        }
-
-	        if (database != null)
-	            database.close();
-
-	        return database != null;
-	    }
-
-	    /**
-	     * Copies your database from your local assets-folder to the just created empty database in the
-	     * system folder, from where it can be accessed and handled.
-	     * This is done by transferring bytestream.
-	     * */
-	    private void copyDatabase() throws IOException{
-	        //Open your local db as the input stream
-	        InputStream myInput = mContext.getAssets().open(DB_NAME);
-
-	        //Open the empty db as the output stream
-	        OutputStream myOutput = new FileOutputStream(getPath());
-
-	        //transfer bytes from the inputfile to the outputfile
-	        byte[] buffer = new byte[1024];
-	        int length;
-	        while ((length = myInput.read(buffer)) > 0) {
-	            myOutput.write(buffer, 0, length);
-	        }
-
-	        //Close the streams
-	        myOutput.flush();
-	        myOutput.close();
-	        myInput.close();
-	    }
-	    
-	    private String getPath() {
-	    	return mContext.getFilesDir().getPath() + DB_NAME;
-	    }
-
-	    public SQLiteDatabase openDatabase() throws SQLException{
-	        //Open the database	      
-	    	if (mDatabase == null) {
-	    		createDatabase();
-	    		mDatabase = SQLiteDatabase.openDatabase(getPath(), null,  SQLiteDatabase.OPEN_READONLY);
-	    	}
-	        return mDatabase;
-	    }
-
-	    @Override
-	    public synchronized void close() {
-	            if(mDatabase != null)
-	                mDatabase.close();
-
-	            super.close();
-	    }
-
-	    @Override
-	    public void onCreate(SQLiteDatabase db) { }
-
-	    @Override
-	    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            createNewDatabase();
+        try {
+            database = SQLiteDatabase.openDatabase(getPath(), null, SQLiteDatabase.OPEN_READONLY);
+        } catch(SQLiteException e) {
+            Log.e(this.getClass().toString(), "Error while checking db");
         }
 
-	    // Add your public helper methods to access and get content from the database.
-	    // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
-	    // to create adapters for your views.
+        if (database != null)
+            database.close();
+
+        return database != null;
+    }
+
+    /**
+     * Copies your database from your local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transferring bytestream.
+     * */
+    private void copyDatabase() throws IOException {
+        //Open your local db as the input stream
+        InputStream myInput = mContext.getAssets().open(DB_NAME);
+
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(getPath());
+
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    private String getPath() {
+        return mContext.getFilesDir().getPath() + DB_NAME;
+    }
+
+    public SQLiteDatabase openDatabase() throws SQLException{
+        //Open the database
+        if (mDatabase == null) {
+            createDatabase();
+            mDatabase = SQLiteDatabase.openDatabase(getPath(), null,  SQLiteDatabase.OPEN_READONLY);
+        }
+        return mDatabase;
+    }
+
+    @Override
+    public synchronized void close() {
+        if (mDatabase != null)
+            mDatabase.close();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) { }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        createNewDatabase();
+    }
+
+    // Add your public helper methods to access and get content from the database.
+    // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
+    // to create adapters for your views.
 
     public String getText(String fileName, TTSEngine.Language language) {
         String languageCode = getLocaleCode(language);
